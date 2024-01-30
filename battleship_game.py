f454920b7050add80888af75e95e63c8f7e8d8c0
Sanaz Zaher <sanaz.zaher@hotmail.com>
2024-01-30 15:28:51 +0100

    A grid of 10x 10 is finished with nested for loops

--- battleship_game.py
+++ battleship_game.py
@@ -5,19 +5,22 @@ import random
 def battelship_map (ship_position):
  """to make a 10 x 10 grid where the ships will be placed in"""
 
-grid =[] # empty list to store the rows and coloms
+grid =[]           # empty list to store the rows and coloms
 
-row_size = 10 # setting variable for row
-colom_size = 10 # setting variable to coloc
+row_size = 10      # setting variable for row 
+colom_size = 10    # setting variable to coloc
 
-
-for row in range (row_size): # 2 nested for loops to make the grid
-    row = [] # creating an empty array
-    for colom in range (colom_size):
+for row in range (row_size):     # 2 nested for loops to make the grid
+    row = []                                    
+    for _ in range (colom_size):
+        
         row.append (".")
     grid.append(row)
+
 for row in grid:
-    print("".join(row))
+    print(" ".join(row))
+
+
 
 #step 2 : User places their ships on the grid
 # step 3 : Pc places its ships on the grid
